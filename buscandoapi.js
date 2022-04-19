46f53be2c46c62a125794c1b54d882348090a6c8
omarcus212 <marcusvinniciusouza@gmail.com>
2022-04-18 21:46:52 -0300

    API finalizado

--- buscandoapi.js
+++ buscandoapi.js
@@ -17,20 +17,34 @@ return data;
 
 
 
-function criarimg({image, biography}){
+function criarimg({image, biography, powerstats,name}){
 
     const card = document.createElement("div");
+    console.log(powerstats)
 
-       card.classList.add('cardheroi');
+       card.classList.add('.card');
     
        card.innerHTML = `
-    
-       <div class="cardheroi">
-       
-           <h3>${biography["full-name"]}</h3>
-           <img src="${image.url}" class="front"></img>
+       <div class="container">
+       <div class="card">
+            <div class="imgbx">
+                <h1>${name}</h1>
+                <img src="${image.url}" alt="">
+                <h3>  Read more..</h3>
+            </div>
+            <div class="contet">
+                <h2>${biography['full-name']}</h2>
+                
+                <p>first-apparence: ${biography['full-name']}</p>
+                <p> intelligence : ${powerstats.intelligence}</p>
+                <p> combat : ${powerstats['combat']}</p>
+                <p> power : ${powerstats['power']}</p>
+                <p>speed : ${powerstats['speed']}</p>
+                <p>strength : ${powerstats['strength']}</p>
+            </div>
+          </div>
+          </div>
            `
-
 
            return card;
 };
@@ -39,13 +53,15 @@ function criarimg({image, biography}){
 async function imgheroi(){
 
     const nomeheroi = document.getElementById('heroi').value;
-    const container = document.getElementById("imagem-container");
+    const container = document.getElementById("container");
 
     const reposta = await pesquisandoHeroi(nomeheroi);
   
     const tagimg = reposta.results.map(criarimg);
+
+    const bla = reposta.results;
     
-   console.log(tagimg);
+   console.log(bla);
   
    container.replaceChildren(...tagimg);
 
